b359ff1773f7cfcdddf9b5f8bd629809821f77f6
yicheng <yichengzhang023@gmail.com>
2019-03-30 22:25:07 +1100

    insertOne -> insertMany

--- mongodb.js
+++ mongodb.js
@@ -7,15 +7,35 @@ const databaseName = "task_manager"
 MongoClient.connect(connectionURL, {
     useNewUrlParser: true
 }, (error, client) => { //avoid parser deprecated.
-    if (error){
+    if (error) {
         return console.log("unable to connect ot the server")
     }
     const db = client.db(databaseName)
-    db.collection("users").insertOne(
-        {
-            'name': 'Yicheng',
-            'Age': 27
-        }
-    )
+    // db.collection("users").insertOne({
+    //     'name': 'Yicheng',
+    //     'Age': 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('unable to insert user')
+    //     }
+    //     console.log(result.ops) //return array of insert results
+
+    // })
+
+    // db.collection('users').insertMany([{
+    //     'name': 'Harry',
+    //     'age': 27
+    // }, {
+    //     'name': 'Yolanda',
+    //     'age': 27
+    // }], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert these documents')
+    //     }
+    //     console.log(result.ops)
+    // })
+
+    db.collection('users').find({})
+
     // console.log("connect successfully!")
 })
